dd4f8ee8da130d74a6b88ded992bc148913495c7
Prince Jain <prince.jain@sunrichtech.com>
2022-08-03 23:41:58 +0530

    little changes in aliigments

--- src/Components/Cart Section/Cart.js
+++ src/Components/Cart Section/Cart.js
@@ -7,25 +7,29 @@ import sharukh from '../../Asset/download.jpg'
 export default function Cart(props) {
     var price1 = 69.99;
     var price2 = 69.99;
-    const style={
-        width:"4rem",
-        height:"4rem"
+    const style = {
+        width: "4rem",
+        height: "4rem"
     }
     return (
         <>
-<div className='row '>
-    <div className='col-2 ml-auto '>
-    <i className="fa fa-shopping-cart border border-dark p-3 text-end"  style={{borderRadius: "2rem"}}></i>
-    </div>
-    <div className='col-4 align-content-center'><hr className='text-secondary'/></div>
-    <div className='col-4 '><i className="fa fa-truck border border-dark bg-success p-3" style={{borderRadius: "2rem"}}></i></div>
-</div>
-        
-            <p className="font-weight-bold"> YOUR CARTS</p>
-            <User style={style} id="98413" img={sachin} product='T-shirt' message="Summer vibes" price={price1} />
-            <User style={style} img={sharukh} product='Sherbani' message="Marrige vibes" price={price2} />
+            <div className='row '>
+                <div className='col-2 ml-auto '>
+                    <i className="fa fa-shopping-cart border border-dark p-3 text-end" style={{ borderRadius: "2rem" }}></i>
+                </div>
+                <div className='col-4 align-content-center'><hr className='text-secondary' /></div>
+                <div className='col-4 '><i className="fa fa-truck border border-dark bg-success p-3" style={{ borderRadius: "2rem" }}></i></div>
+            </div>
             <div className='row'>
-                <div className='col col-6 m-auto'>
+                <div className='col col-12'>
+                    <p className="font-weight-bold"> YOUR CARTS</p>
+                    <User style={style} id="98413" img={sachin} product='T-shirt' message="Summer vibes" price={price1} />
+                    <User style={style} img={sharukh} product='Sherbani' message="Marrige vibes" price={price2} />
+                </div>
+            </div>
+
+            <div className='row'>
+                <div className='col col-9 mx-auto'>
                     <button className='btn   px-5 py-3 ' style={{ borderRadius: "2rem", backgroundColor: "#e5e5e5" }}> Total Cost &nbsp; &nbsp; &nbsp;{price1 + price2} </button>
                 </div>
                 <div className='w-100'></div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,35 +8,37 @@ export default function Home() {
 
     return (
         <>
-            <div className='row my-2'>
-                <div className='col col-lg-4 col-md-6 col-sm-12'>
+            <div className='row'>
+                <div className='col col-lg-4 col-md-12'>
                     <h4>Shipping and Payment</h4>
                     <Form />
                     <a className='mt-5 py-5 text-dark' href='/'>
                         <i className="fa fa-arrow-left"></i> Back</a>
                 </div>
-
-                <div className='col col-lg-4 col-md-12 col-sm-12'>
-                    <Payment />
-
+                <div className='col col-lg-4 col-md-12'>
+                <Payment />
 
                 </div>
-                <div className='col col-lg-4 col-md-12 col-sm-12'>
-                    <Cart />
-
-
+                <div className='col col-lg-4 col-md-12'>
+                <Cart />
                 </div>
-                <div className=' ml-auto col col-lg-2 col-md-3 col-sm-5 my-2 '>
-                    <button className='btn btn-outline-dark p-3' style={{ borderRadius: "2rem" }}>Continue Shopping</button>
-                </div>
-                <div className=' col col-md-4 col-lg-3 col-sm-6 my-2 '>
-                    <button className='btn btn-success p-3' style={{ borderRadius: "2rem" }}>Proceed To Payment</button>
-
-                </div>
-
-
-
             </div>
+            <div className='row'>
+                <div className='col col-lg-4 col-md-6 my-2 ml-auto'>
+                <button className='btn btn-outline-dark py-3 px-5' style={{ borderRadius: "2rem" }}>Continue Shopping</button>
+                </div>
+                <div className='col col-lg-4 col-md-6 my-2 '>
+                <button className='btn btn-success py-3 px-5' style={{ borderRadius: "2rem" }}>Proceed To Payment</button>
+                </div>
+            </div>
+
+
+
+
+            
+
+
+
         </>
 
 
